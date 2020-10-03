f426488f26fd9a8f08f1baa8ba813d354a610f3b
Scott Juneau <dsjuneau@gmail.com>
2020-10-03 10:18:41 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# practice-15
+# Factory Functions and Constructor Functions
+
+Write a function that produces an object with:
+
+1.  2 properties
+2.  One method
+
+Call the function
---
+++ app.js
@@ -0,0 +1,31 @@
+// factory function
+
+const fFunction = (a, b) => {
+  return {
+    num1: parseInt(a),
+    num2: parseInt(b),
+    num3: 300,
+    addNumber() {
+      return this.num1 + this.num2 + this.num3;
+    },
+  };
+};
+
+const newOjbect = fFunction(process.argv[2], process.argv[3]);
+
+console.log(newOjbect);
+console.log(newOjbect.addNumber());
+
+function OFunction(a, b) {
+  this.num1 = parseInt(a);
+  this.num2 = parseInt(b);
+  this.num3 = 300;
+  this.addNumber = () => {
+    return this.num1 + this.num2 + this.num3;
+  };
+}
+
+const nO2 = new OFunction(process.argv[2], process.argv[3]);
+
+console.log(nO2);
+console.log(nO2.addNumber());
