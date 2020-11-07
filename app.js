4441cfc80a857d27712986f4a4fb6ffd6c144e10
Scott Juneau <dsjuneau@gmail.com>
2020-11-07 17:33:19 -0600

    updated function constructor

--- app.js
+++ app.js
@@ -16,14 +16,17 @@ const newOjbect = fFunction(process.argv[2], process.argv[3]);
 console.log(newOjbect);
 console.log(newOjbect.addNumber());
 
+// Function Constructor
+
 function OFunction(a, b) {
   this.num1 = parseInt(a);
   this.num2 = parseInt(b);
   this.num3 = 300;
-  this.addNumber = () => {
-    return this.num1 + this.num2 + this.num3;
-  };
 }
+
+OFunction.prototype.addNumber = function () {
+  return this.num1 + this.num2 + this.num3;
+};
 
 const nO2 = new OFunction(process.argv[2], process.argv[3]);
 
